8dbf1f55508ce1746583f736ab8cdc7082228b65
Enrique <alvarez.enrique0508@gmail.com>
2018-09-15 08:09:15 -0500

    Upgrade user suscription

--- cloneNetflix/src/Const.js
+++ cloneNetflix/src/Const.js
@@ -1,3 +1,8 @@
 module.exports = {
-    APP_SECRET : "jsaksdasidpaoqpi91208j"
+    APP_SECRET : "jsaksdasidpaoqpi91208j",
+    PRICES : {
+        FREE:0,
+        GOLD:99,
+        PREMIUM:149
+    }
 }
--- cloneNetflix/src/resolvers/Mutation.js
+++ cloneNetflix/src/resolvers/Mutation.js
@@ -1,13 +1,44 @@
 const bcrypt = require('bcrypt');
 const jsonwebtoken = require('jsonwebtoken');
-const { APP_SECRET } = require("../Const");
+const { APP_SECRET, PRICES } = require("../Const");
+
 const getID = `{ id }`;
+
+const queryUser = `{
+    id,
+    email,
+    name,
+    lastname,
+    birth_date,
+    suscription{
+        id,
+        end_date,
+        suscription_type,
+        price
+    }
+}`
+
+const { getUserId } = require('../utils/utils')
+
+
+Date.prototype.addDays = function(days){
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
 
 async function signup(parent,args,context,info) {
     const password = await bcrypt.hash(args.password,10);
 
-    const user = context.db.mutation.createUser(
-       {data:{...args,password},getID}
+    const user = await context.db.mutation.createUser(
+       {data:{...args,password,suscription:{
+           create:{
+               suscription_type : "FREE",
+               price:0,
+               end_date: new Date().addDays(30) 
+           }
+       }
+    }},queryUser
     )
 
     const token = jsonwebtoken.sign({userId:user.id},APP_SECRET);
@@ -42,7 +73,31 @@ async function login(parent,args,context,info){
 
 }
 
+async function upgradeSuscription (context,args,info,parent) {
+    let user = getUserId(context);
+    let days = (args.suscription_type == "PREMIUM") ? 90 : 30;
+    let updateUser = await context.db.mutation.updateUser(
+        {
+            data: {
+                suscription:{
+                    update:{
+                        suscription_type:args.suscription_type,
+                        end_date : new Date().addDays(days),
+                        price : PRICES[args.suscription_type]
+                    }
+                }
+                
+            },
+            where:{
+                id:user
+            }
+        },queryUser)
+
+    return suscription;
+  }
+
 module.exports = {
     signup,
-    login
+    login,
+    upgradeSuscription
 }
